0ef2b18df7e807c35fa636e76e8809bc3a477f06
KhanyaMzi <khanyamzingelwa1@gmail.com>
2023-06-15 04:14:58 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,8 +2,9 @@
 /**
  * main - A program that prints a line using the printf function
  *
- * Return; Always 0 (Success)
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n")
